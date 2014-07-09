016198b285dbe7a1d4bc6baeb68605e2fbeee044
spood <speedtouch11@gmail.com>
2014-07-08 19:12:34 -0700

    disabled twitch autoplay

--- public/vendor/twitch/twitch-api.js
+++ public/vendor/twitch/twitch-api.js
@@ -47,7 +47,7 @@ function buildEmbedPlayer(playerType, channel) {
   }
   else {
     // Flash player
-    twitchPlayer = "<object type='application/x-shockwave-flash' height='378' width='620' id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel=hebo' bgcolor='#000000'><param name='allowFullScreen' value='true' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' /><param name='flashvars' value='hostname=www.twitch.tv&channel=" + channel + "&auto_play=true&start_volume=25' /></object>"
+    twitchPlayer = "<object type='application/x-shockwave-flash' height='378' width='620' id='live_embed_player_flash' data='http://www.twitch.tv/widgets/live_embed_player.swf?channel=hebo' bgcolor='#000000'><param name='allowFullScreen' value='true' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='movie' value='http://www.twitch.tv/widgets/live_embed_player.swf' /><param name='flashvars' value='hostname=www.twitch.tv&channel=" + channel + "&auto_play=false&start_volume=25' /></object>"
   }
   return twitchPlayer;
 }
